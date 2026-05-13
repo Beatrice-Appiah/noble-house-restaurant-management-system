bee58bb052833811cc9f6258218d09233cbd90cd
Beatrice Appiah <beatricenaameryeaappiah@gmail.com>
2026-05-13 22:42:02 +0000

    Update port for Railway deployment

--- server.js
+++ server.js
@@ -41,6 +41,8 @@ app.get('/inventory', (req, res) => {
   res.sendFile(__dirname + '/views/inventory.html');
 });
 
-app.listen(3000, () =>
-  console.log('Server running on http://localhost:3000')
-);
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
